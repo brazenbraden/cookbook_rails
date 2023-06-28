21694638da21066ceafb80f1ef355acc5022eb0f
Braden King <braden@brazenbraden.com>
2023-06-28 12:43:17 +0100

    Adds IngredientType files and association to Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,6 +3,7 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+  belongs_to :ingredient_type
 
   validates :name, presence: true, uniqueness: true
 end
